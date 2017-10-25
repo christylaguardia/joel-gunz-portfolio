900648d29470912a4c0e2977bf2b9699994945e9
christylaguardia <christinelaguardia@gmail.com>
2017-10-25 02:24:44 -0700

    added contact info

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,8 @@ export default function About() {
   return (
     <div>
       <p>Hi I'm Joel and I like to ask interesting questions!</p>
+      <p>503-901-6156</p>
+      <p><a href="mailto:joel.gunz@gmail.com">joel.gunz@gmail.com</a></p>
     </div>
   );
 }
