cac1dbcc2cdf6445fa42081436e54b6ee4f3f5d6
christylaguardia <christinelaguardia@gmail.com>
2018-02-04 00:40:44 -0800

    working on blogger api

--- blogger/scraper.js
+++ blogger/scraper.js
@@ -4,12 +4,14 @@ require('dotenv').config();
 
 const API_KEY = 'AIzaSyCjdl3z9EJLUsxPHlxU1jpD4VKx1MwpHpc';
 const BLOG_ID = '7219214733140697041';
+let apiUrl = `https://www.googleapis.com/blogger/v3/blogs/${BLOG_ID}/posts?key=${API_KEY}`;
 
 const timestamp = new Date().toISOString();
 const dir = `data/${timestamp}`;  
 
+// TODO: SET INTERVAL
+
 function getBlogArticles(nextPageToken) {
-  let apiUrl = `https://www.googleapis.com/blogger/v3/blogs/${BLOG_ID}/posts?key=${API_KEY}`;
   if (nextPageToken) apiUrl = `${apiUrl}&pageToken=${nextPageToken}`;
 
   if (!fs.existsSync(dir)) {
@@ -25,7 +27,7 @@ function getBlogArticles(nextPageToken) {
       blogs.forEach(blog => {
         fs.writeFile(`${dir}/${blog.id}.json`, JSON.stringify(blog), (err) => {
           if (err) throw err;
-          console.log(`The file ${blog.id} has been saved!`);
+          console.log(`The file ${blog.id} has been saved! Updated: ${blog.updated}`);
         });
       });
 
@@ -34,4 +36,34 @@ function getBlogArticles(nextPageToken) {
     .catch(err => console.log('ERROR', err));
 }
 
-getBlogArticles();
+// getBlogArticles();
+
+
+// THIS DOESN'T WORK
+function getLatestBlogArticle(nextPageToken, blogs=[]) {
+  console.log('getting posts...', nextPageToken);
+  if (nextPageToken) apiUrl = `${apiUrl}&pageToken=${nextPageToken}`;
+
+  superagent
+    .get(apiUrl)
+    .then(res => {
+      const newPageToken = res.body.nextPageToken;
+      const newBlogs = res.body.items;
+      // newBlogs.forEach
+      console.log('newBlogs', newBlogs);
+      blogs.push(newBlogs);
+      
+      if (newPageToken) return getLatestBlogArticle(newPageToken, blogs);
+
+      console.log(blogs);
+      
+
+      const latest = blogs.sort((a, b) => a.updated - b.updated)[0];
+      console.log(latest);
+      // return latest;
+
+    })
+    .catch(err => console.log('ERROR', err));
+}
+
+// getLatestBlogArticle();
