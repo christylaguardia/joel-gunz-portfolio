3cdc1575ffc6c4dbdd18d73d51db4d115f7f6c71
christylaguardia <christinelaguardia@gmail.com>
2018-02-04 00:39:52 -0800

    added categories to header

--- src/App/Header.js
+++ src/App/Header.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-function Categories() {
-  return (
-    <div>
-      <p><Link to="/">Joel Gunz</Link></p>
-      <p><Link to="/about">About</Link></p>
-      <p><Link to="/login">Login</Link></p>
-    </div>
-  );
+class Header extends React.Component {
+
+  render() {
+    const { categories } = this.props;
+
+    return (
+      <ul>
+        <li><Link to="/">Joel Gunz</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        {categories && categories.map((c, i) => {
+          return (
+            <li key={i}>
+              <Link to={`/${c.toLowerCase()}`}>{c}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
 
-export default Categories;
+export default connect(
+  state => ({ categories: state.data.categories }),
+  null
+)(Header);
