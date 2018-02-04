9a720b0d38b9f2d4183429a50c9b84266e9a4f64
christylaguardia <christinelaguardia@gmail.com>
2018-02-04 00:36:23 -0800

    added basic css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store/store';
 import App from './app/App';
+import './index.css';
 
 render(
   <Provider store={store}>
