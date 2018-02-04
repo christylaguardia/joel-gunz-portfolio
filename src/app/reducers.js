f053933b04d6f5dca2b7ae91fc55508c1e2537be
christylaguardia <christinelaguardia@gmail.com>
2018-02-04 00:40:20 -0800

    added reducers

--- src/app/reducers.js
+++ src/app/reducers.js
@@ -1,26 +1,9 @@
-export const FIREBASE_CONFIG = 'FIREBASE_CONFIG';
-export const FIREBASE_REF = 'FIREBASE_REF';
-export const FETCHED_PAGES = 'FETCHED_PAGES';
 export const LOADING = 'LOADING';
 export const LOADED = 'LOADED';
-
-export const pages = (state = [], { type, payload }) => {
-  switch (type) {
-    case FETCHED_PAGES:
-      return payload;
-    default:
-      return state;
-  }
-};
-
-export const categories = (state = [], { type, payload }) => {
-  switch (type) {
-    case FETCHED_PAGES:
-      return payload;
-    default:
-      return state;
-  }
-};
+export const FETCHED_DATA = 'FETCHED_DATA';
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const ERROR = 'ERROR';
 
 export const loading = (state = false, { type }) => {
   switch (type) {
@@ -28,6 +11,35 @@ export const loading = (state = false, { type }) => {
       return true;
     case LOADED:
       return false;
+    default:
+      return state;
+  }
+};
+
+export const error = (state = null, { type, payload }) => {
+  switch (type) {
+    case ERROR:
+      return payload;
+    default:
+      return state;
+  }
+};
+
+export const data = (state = {}, { type, payload }) => {
+  switch (type) {
+    case FETCHED_DATA:
+      return payload;
+    default:
+      return state;
+  }
+};
+
+export const user = (state = {}, { type, payload }) => {
+  switch (type) {
+    case LOGIN:
+      return payload;
+    case LOGOUT:
+      return undefined;    
     default:
       return state;
   }
